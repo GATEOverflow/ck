434813b408c4394fb70ac5b1aedea5d9e1039d1c
Arjun Suresh <arjunsuresh1987@gmail.com>
2023-04-27 12:30:07 +0100

    Added gh action for readme update

--- tests/script/process_readme.py
+++ tests/script/process_readme.py
@@ -1,2 +1,17 @@
 import sys
-print (sys.argv[1:])
+import os
+import cmind as cm
+import check as checks
+import json
+
+files=sys.argv[1:]
+
+for file in files:
+  if not file.endswith("_cm.json"):
+    continue
+  script_path = os.path.dirname(file)
+  f = open(file)
+  data = json.load(f)
+  automation_uid = data['automation_uid']
+  r = cm.access({'action':'doc', 'automation':f'script,{automation_uid}', 'quiet': 'yes'})
+  checks.check_return(r)
